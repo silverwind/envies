88408df432e64d453d335ccd213a75bd19d403ab
silverwind <me@silverwind.io>
2025-07-03 08:49:08 +0200

    check windows env var

--- index.test.ts
+++ index.test.ts
@@ -8,7 +8,7 @@ test("works", () => {
     baz"
   `);
   expect(config.QUX).toMatchInlineSnapshot(`undefined`);
-  expect(config.USER).toBeTruthy();
+  expect(config.USER || config.USERNAME).toBeTruthy();
   expect("FOO" in config).toEqual(true);
   expect(Object.keys(config).length).toBeGreaterThanOrEqual(2);
 });
